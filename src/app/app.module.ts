14567bed436b1a37f7bf6c9c9f5569f77323252b
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-02-01 08:24:07 -0600

    new scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NextDirective } from './next.directive';
+import { PrevDirective } from './prev.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NextDirective,
+    PrevDirective
   ],
   imports: [
     BrowserModule
