af9fd7b1d0d263e190f09303c60506fdc163d9a7
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-02-28 22:53:15 -0600

    added dropdowns for about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  isMenuOpened: boolean = false;
 
+  toggleMenu(): void {
+    this.isMenuOpened = !this.isMenuOpened;
+  }
+
+  clickedOutside(): void {
+    this.isMenuOpened = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule } from '@angular/router';
 
@@ -8,15 +9,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ResumeComponent } from './resume/resume.component';
-
-
-
 import { NextDirective } from './resume/next.directive';
 import { PrevDirective } from './resume/prev.directive';
-import { CommonModule } from '@angular/common';
+
 import { HomeComponent } from './home/home.component';
 import { CarouselModule } from './home/carousel/carousel.module';
+
 import { AboutComponent } from './about/about.component';
+import { ClickOutsideDirective } from './about/clickOutside.directive';
 import { HeartComponent } from './about/heart/heart.component';
 import { LeesComponent } from './about/lees/lees.component';
 import { AureliosComponent } from './about/aurelios/aurelios.component';
@@ -30,6 +30,7 @@ import { AureliosComponent } from './about/aurelios/aurelios.component';
     ResumeComponent,
     HomeComponent,
     AboutComponent,
+    ClickOutsideDirective,
     HeartComponent,
     LeesComponent,
     AureliosComponent
