1483dc0666f349558c88a2008c08e25685068f2a
Aidan-M-Monner <aidan.m.monner@gmail.com>
2023-03-13 12:09:12 -0500

    Home component now has images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,20 +9,28 @@ export class HomeComponent {
 
   images = [
     {
-      imageSrc: 'https://64.media.tumblr.com/tumblr_lz46chSuSk1qh5juxo1_1280.jpg',
-      imageAlt: 'Finn-bot'
+      imageSrc: '/assets/home/Aidan-Monner.png',
+      imageAlt: 'main-img'
     },
     {
-      imageSrc: 'https://64.media.tumblr.com/171193b48d6e1482d73a813daf1942eb/cadb65208b1c3d8c-71/s540x810/c85a0da9ad19c6a3202e99d93813366515f0a08f.jpg',
-      imageAlt: 'BMO-bubbles'
+      imageSrc: '/assets/home/Aidan-Monner-Beasley.png',
+      imageAlt: 'dog'
     },
     {
-      imageSrc: 'https://www.desktopbackground.org/download/1440x900/2015/01/01/880483_adventure-time-wallpaper-tumblr-walleo-co_1920x1080_h.jpg',
-      imageAlt: 'Finn-Jake-quotes'
+      imageSrc: '/assets/home/Aidan-Monner-drawing.png',
+      imageAlt: 'drawing'
     },
     {
-      imageSrc: 'https://wallpaperaccess.com/full/830205.png',
-      imageAlt: 'Adventure-Time'
+      imageSrc: '/assets/home/Aidan-Monner-Aquarium.png',
+      imageAlt: 'aquarium'
+    },
+    {
+      imageSrc: '/assets/home/Aidan-Monner-Monado.png',
+      imageAlt: 'monado'
+    },
+    {
+      imageSrc: '/assets/home/Aidan-Monner-Nostalgia-Critic.png',
+      imageAlt: 'nostalgia'
     },
   ]
 }
